0e6a011868b3f25561a5c77f951c50c0c6e881c8
Paul <paul.chabas@etu.univ-amu.fr>
2019-09-13 11:37:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+    <title>Premier exo</title>
+</head>
+<body>
+<?php
+echo'<strong>Voici mon premier programme PHP</strong><br/>'."\n";echo'C\'est pas mal non ? <br/>';
+?>
+</body>
+</html>

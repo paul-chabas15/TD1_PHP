6133875c04a18bf672727c41fb16c5bb25e1c57a
Paul <paul.chabas@etu.univ-amu.fr>
2019-09-13 11:52:53 +0200

    Grand deux

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE HTML>
 <html lang="fr">
 <head>
-    <title>Premier exo</title>
+    <title>2</title>
 </head>
 <body>
-<?php
-echo'<strong>Voici mon premier programme PHP</strong><br/>'."\n";echo'C\'est pas mal non ? <br/>';
-?>
+    <?php
+        echo'<strong>Voici mon premier programme PHP</strong><br/>'."\n";
+        echo'C\'est pas mal non ? <br/>';
+        $var1 = 6;
+        $var2 = 1.3;
+        $var3 = 'Variable3';
+        echo 'var1 = 6, var2 = 1.3, var3 = Variable3';
+        echo "$var1 + $var2", 'echo "$var1 + $var2";';
+        echo $var1 + $var2, 'echo $var1 + $var2';
+        echo $var1 + $var3, 'echo $var1 + $var3';
+    ?>
 </body>
 </html>
